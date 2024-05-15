87bc0421165764140267a01e809f894a04d89dd7
sumanthusorab@gmail.com <sumanthusorab@gmail.com>
2024-05-15 06:16:07 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +12,14 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.text_box_1.text
+    weight = self.text_box_2.text
+    address = self.text_area_1.text
+    personal = self.check_box_1.checked
+    anvil.server.call('submit',name,weight,address,personal)
+    Notification("data has been submitted successfully").show()
+    
+    
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,17 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+@anvil.server.callable
+def submit(name,weight,personal,address):
+  app_tables.gym.add_row(name=name,weight=weight,address=address,perosnal=personal)
+  return 42
+
